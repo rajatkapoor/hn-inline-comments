b790b2f10cdf7ff1cc46880e58a4120a2e17d064
Rajat <rajat100493@gmaill.com>
2022-03-27 22:08:04 +0530

    Added remark directive plugin to display inline comments
    decided on the format of the hashnode comment in markdown

---
+++ plugins/hashnodeComment.plugin.js
@@ -0,0 +1,18 @@
+import { visit } from "unist-util-visit";
+import { h } from "hastscript";
+
+const hashnodeCommentPlugin = () => {
+  return (tree) => {
+    visit(tree, (node) => {
+      if (node.type === "textDirective" && node.name === "hn-comment") {
+        const data = node.data || (node.data = {});
+        const hast = h(node.name, node.attributes);
+
+        data.hName = hast.tagName;
+        data.hProperties = hast.properties;
+      }
+    });
+  };
+};
+
+export default hashnodeCommentPlugin;
