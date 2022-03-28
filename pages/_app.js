158515b937e09162df7f599fc7dc3a6d1800af15
Rajat <rajat100493@gmaill.com>
2022-03-28 18:00:24 +0530

    Minor beautification

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { Box, ChakraProvider } from "@chakra-ui/react";
+import { theme } from "../theme";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider resetCSS={false}>
-      <Component {...pageProps} />
+    <ChakraProvider resetCSS={false} theme={theme}>
+      <Box minH={"100vh"} bg="blue.900" p={16}>
+        <Box height={"100%"} rounded={"xl"} bg="white" padding={16}>
+          <Component {...pageProps} />
+        </Box>
+      </Box>
     </ChakraProvider>
   );
 }
---
+++ theme.js
@@ -0,0 +1,12 @@
+import { extendTheme } from "@chakra-ui/react";
+
+export const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        margin: 0,
+        padding: 0,
+      },
+    },
+  },
+});
