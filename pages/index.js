ee9bee682b858856057772c5cebc3b5b5f3c4ac1
Rajat <rajat100493@gmaill.com>
2022-03-28 19:25:01 +0530

    Getting posts from firestore and rendering them

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,44 @@
+import { Button, Heading, HStack, Stack, Text } from "@chakra-ui/react";
+import { getDocs } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { posts as postsCollection } from "../utils/firebase";
+
 export default function Home() {
-  return <div>Add stuff here</div>;
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    getDocs(postsCollection).then((res) => {
+      const docs = res.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+      setPosts(docs);
+    });
+  }, []);
+
+  return (
+    <Stack>
+      <Heading>Your posts</Heading>
+      <Stack>
+        {posts.map((post) => {
+          return (
+            <HStack key={post.id} justifyContent="space-between">
+              <Stack>
+                <Heading size={"md"}>{post.id}</Heading>
+                <Text isTruncated>{post.content}</Text>
+              </Stack>
+              <HStack>
+                <Button
+                  colorScheme={"blue"}
+                  variant={"outline"}
+                  rounded={"3xl"}
+                >
+                  Continue writing
+                </Button>
+                <Button variant={"ghost"} colorScheme="red">
+                  Delete
+                </Button>
+              </HStack>
+            </HStack>
+          );
+        })}
+      </Stack>
+    </Stack>
+  );
 }
